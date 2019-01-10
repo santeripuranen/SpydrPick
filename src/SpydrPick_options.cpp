2966a24e2b4a85cf1f50ea2a425785bba29ef020
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2019-01-10 09:37:34 +0200

    Change spelling of project name; Correct version string; Add options

--- src/SpydrPick_options.cpp
+++ src/SpydrPick_options.cpp
@@ -1,4 +1,4 @@
-/** @file spydrpick.cpp
+/** @file SpydrPick_options.cpp
 
 	Copyright (c) 2017-2018 Santeri Puranen.
 
@@ -19,37 +19,40 @@
 	$Id: $
 */
 
-#include "spydrpick.h"
-#include "spydrpick_version.h"
+#include "../include/SpydrPick_version.h"
+#include "SpydrPick_options.h"
 
 namespace spydrpick {
 
-std::ostream* spydrpick::s_out = nullptr;
-std::ostream* spydrpick::s_err = nullptr;
-bool spydrpick::s_verbose = false;
+std::ostream* SpydrPick_options::s_out = nullptr;
+std::ostream* SpydrPick_options::s_err = nullptr;
+bool SpydrPick_options::s_verbose = false;
 
 #ifndef SPYDRPICK_NO_TBB // Threading with Threading Building Blocks
-int spydrpick::s_threads = -1;
+int SpydrPick_options::s_threads = -1;
 #else
-int spydrpick::s_threads = 1;
+int SpydrPick_options::s_threads = 1;
 #endif // SPYDRPICK_NO_TBB
 
-std::string spydrpick::s_options_string;
+double SpydrPick_options::s_mi_threshold = -1.;
+double SpydrPick_options::s_mi_pseudocount = 0.5;
 
-const std::string spydrpick::s_title_string(
-	  std::string("spydrpick: Genome-wide epistasis analysis with MI-ARACNE.\n")
+std::string SpydrPick_options::s_options_string;
+
+const std::string SpydrPick_options::s_title_string(
+	  std::string("SpydrPick: Genome-wide epistasis analysis with MI-ARACNE.\n")
 );
 
-const std::string spydrpick::s_usage_string(
-	  std::string("Usage: spydrpick") /*+ std::string(argv[0])*/ + " [options] <alignmentfile> [-o <outputfile>]\nOption '--help' will print a list of available options.\n"
+const std::string SpydrPick_options::s_usage_string(
+	  std::string("Usage: SpydrPick") /*+ std::string(argv[0])*/ + " [options] <alignmentfile> [-o <outputfile>]\nOption '--help' will print a list of available options.\n"
 );
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-const std::string spydrpick::s_version_string(
-	std::string("spydrpick version ") + std::to_string(spydrpick_version::s_MajorVersion) + "." + std::to_string(spydrpick_version::s_MinorVersion) + "." + std::to_string(spydrpick_version::s_SubminorVersion)
-	+ " revision " + TOSTRING(GIT_BRANCH) + "-" + TOSTRING(GIT_COMMIT_HASH) + " / " +
+const std::string SpydrPick_options::s_version_string(
+	std::string("SpydrPick version ") + std::to_string(spydrpick_version::s_MajorVersion) + "." + std::to_string(spydrpick_version::s_MinorVersion) + "." + std::to_string(spydrpick_version::s_SubminorVersion)
+	+ " | revision " + TOSTRING(SPYDRPICK_GIT_BRANCH) + "-" + TOSTRING(SPYDRPICK_GIT_COMMIT_HASH) + " | " +
 #ifdef __AVX2__
 	"AVX2"
 #elif __AVX__
@@ -62,24 +65,12 @@ const std::string spydrpick::s_version_string(
 	+ " build " + std::string(__DATE__) + " " + std::string(__TIME__)
 );
 
-const std::string spydrpick::s_copyright_notice(
+const std::string SpydrPick_options::s_copyright_notice(
 	std::string("Copyright (c) 2017-2018 Santeri Puranen\nLicensed under the GNU Affero General Public License version 3.\n\n")
 	+ "THIS SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND."
 );
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-const std::string spydrpick::s_long_copyright_notice(
+const std::string SpydrPick_options::s_long_copyright_notice(
 	std::string("Copyright (c) 2017-2018 Santeri Puranen\nLicensed under the GNU Affero General Public License version 3.\n\n")
 	+ "This program is free software: you can redistribute it and/or modify\n"
     + "it under the terms of the GNU Affero General Public License as\n"
@@ -93,54 +84,56 @@ const std::string spydrpick::s_long_copyright_notice(
     + "along with this program. If not, see <http://www.gnu.org/licenses/>."
 );
 
-spydrpick::spydrpick() { this->m_init(); }
-spydrpick::~spydrpick() { }
+SpydrPick_options::SpydrPick_options() { this->m_init(); }
+SpydrPick_options::~SpydrPick_options() { }
 
-spydrpick::spydrpick( std::ostream *out, std::ostream *err )
+SpydrPick_options::SpydrPick_options( std::ostream *out, std::ostream *err )
 {
-	spydrpick::s_out = out;
-	spydrpick::s_err = err;
+	SpydrPick_options::s_out = out;
+	SpydrPick_options::s_err = err;
 	this->m_init();
 }
 
-const std::string& spydrpick::s_get_copyright_notice_string() { return s_copyright_notice; }
-const std::string& spydrpick::s_get_usage_string() { return s_usage_string; }
-const std::string& spydrpick::s_get_version_string() { return s_version_string; }
-const std::string& spydrpick::s_get_title_string() { return s_title_string; }
+const std::string& SpydrPick_options::s_get_copyright_notice_string() { return s_copyright_notice; }
+const std::string& SpydrPick_options::s_get_usage_string() { return s_usage_string; }
+const std::string& SpydrPick_options::s_get_version_string() { return s_version_string; }
+const std::string& SpydrPick_options::s_get_title_string() { return s_title_string; }
 
-bool spydrpick::has_alignment_filenames() const { return !m_alignment_file_names.empty(); }
-const std::vector< std::string >& spydrpick::get_alignment_filenames() const { return m_alignment_file_names; }
+void SpydrPick_options::set_out_stream( std::ostream *out ) { s_out = out->good() ? out : nullptr; }
+void SpydrPick_options::set_err_stream( std::ostream *err ) { s_err = err->good() ? err : nullptr; }
 
-void spydrpick::set_out_stream( std::ostream *out ) { s_out = out->good() ? out : nullptr; }
-void spydrpick::set_err_stream( std::ostream *err ) { s_err = err->good() ? err : nullptr; }
+std::ostream* SpydrPick_options::get_out_stream() { return s_out; }
+std::ostream* SpydrPick_options::get_err_stream() { return s_err; }
 
-std::ostream* spydrpick::get_out_stream() { return s_out; }
-std::ostream* spydrpick::get_err_stream() { return s_err; }
+const std::string& SpydrPick_options::s_get_options_string() { return s_options_string; }
+void SpydrPick_options::m_set_options_string( const std::string& options_string ) { s_options_string = options_string; }
 
-const std::string& spydrpick::s_get_options_string() { return s_options_string; }
-void spydrpick::m_set_options_string( const std::string& options_string ) { s_options_string = options_string; }
+bool SpydrPick_options::verbose() { return ( s_verbose && s_out ); } // be verbose only if we have a valid outstream.
 
-bool spydrpick::verbose() { return ( s_verbose && s_out ); } // be verbose only if we have a valid outstream.
+int SpydrPick_options::threads() { return s_threads; }
 
-int spydrpick::threads() { return s_threads; }
+double SpydrPick_options::get_mi_threshold() { return s_mi_threshold; }
+double SpydrPick_options::get_mi_pseudocount() { return s_mi_pseudocount; }
 
-void spydrpick::m_init()
+void SpydrPick_options::m_init()
 {
 	namespace po = boost::program_options;
 
 	m_general_options.add_options()
 		("help,h", "Print this help message.")
-		("verbose,v", po::bool_switch( &spydrpick::s_verbose )->default_value(spydrpick::s_verbose)->notifier(spydrpick::s_init_verbose), "Be verbose.")
-		("alignmentfile", po::value< std::vector< std::string > >( &m_alignment_file_names )->composing(), "The input alignment filename(s). When two filenames are specified, only inter-alignment links will be probed for.")
+		("version", "Print version information.")
+		("verbose,v", po::bool_switch( &SpydrPick_options::s_verbose )->default_value(SpydrPick_options::s_verbose)->notifier(SpydrPick_options::s_init_verbose), "Be verbose.")
+		("mi-threshold", po::value< double >( &SpydrPick_options::s_mi_threshold )->default_value(SpydrPick_options::s_mi_threshold), "The MI threshold value. Experience suggests that a value of 0.11 is often reasonable. Zero indicates no threshold and negative values will trigger auto-define heuristics.")
+		("mi-pseudocount", po::value< double >( &SpydrPick_options::s_mi_pseudocount )->default_value(SpydrPick_options::s_mi_pseudocount), "The MI pseudocount value.")
 	;
 	m_parallel_options.add_options()
 #ifndef SPYDRPICK_NO_TBB // Threading with Threading Building Blocks
-		("threads,t", po::value< int >( &spydrpick::s_threads )->default_value(spydrpick::s_threads)->notifier(spydrpick::s_init_threads), "Number of threads per MPI/shared memory node (-1=use all hardware threads that the OS/environment exposes).")
+		("threads,t", po::value< int >( &SpydrPick_options::s_threads )->default_value(SpydrPick_options::s_threads)->notifier(SpydrPick_options::s_init_threads), "Number of threads per MPI/shared memory node (-1=use all hardware threads that the OS/environment exposes).")
 #endif // SPYDRPICK_NO_TBB
 	;
 }
 
-void spydrpick::AddOptions( boost::program_options::options_description *opdesc )
+void SpydrPick_options::AddOptions( boost::program_options::options_description *opdesc )
 {
 	namespace po = boost::program_options;
 	opdesc->add(m_general_options);
@@ -148,21 +141,20 @@ void spydrpick::AddOptions( boost::program_options::options_description *opdesc 
 }
 
 /// Check options stored in varmap. Return false if a fatal inconsistency is detected.
-bool spydrpick::CheckOptions( boost::program_options::variables_map *varmap )
+bool SpydrPick_options::CheckOptions( boost::program_options::variables_map *varmap )
 {
 	try
 	{
+		if( varmap->count("version") && s_out )
+		{
+			*s_out << s_get_version_string() << std::endl;
+			exit(EXIT_SUCCESS);
+		}
 		if( varmap->count("help") && s_out )
 		{
 			*s_out << s_title_string << "\n" << s_usage_string << s_options_string << std::endl;
-			spydrpick::Exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);;
 		}
-/*
-		if( s_verbose && s_out )
-		{
-			*s_out << "spydrpick: Being verbose." << std::endl;
-		}
-*/
 		if( !varmap->count("alignmentfile") && s_err )
 		{
 			*s_err << "spydrpick ERROR: No alignment file specified!" << std::endl;
@@ -196,7 +188,7 @@ bool spydrpick::CheckOptions( boost::program_options::variables_map *varmap )
 	return true;
 }
 
-void spydrpick::s_init_verbose( const bool& verbose )
+void SpydrPick_options::s_init_verbose( const bool& verbose )
 {
 	if( verbose && s_out )
 	{
@@ -205,7 +197,7 @@ void spydrpick::s_init_verbose( const bool& verbose )
 }
 
 #ifndef SPYDRPICK_NO_TBB // Threading with Threading Building Blocks
-void spydrpick::s_init_threads( const int& nthreads )
+void SpydrPick_options::s_init_threads( const int& nthreads )
 {
 	if( s_verbose && s_out )
 	{
