9b4e7dc7ff34844b232b2051661b77e2d3858b8d
santeripuranen <26481940+santeripuranen@users.noreply.github.com>
2021-02-08 23:13:14 +0200

    Attempt to fix git branch and commit hash extraction script

--- cmake/git_commit_info.cmake
+++ cmake/git_commit_info.cmake
@@ -2,21 +2,28 @@
 #
 # http://xit0.org/2013/04/cmake-use-git-branch-and-commit-details-in-project/
 
-# Get the current working branch
-execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE ${PROJECT_NAME}_GIT_BRANCH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+get_directory_property( CMDLINEDEFS COMPILE_DEFINITIONS )
 
-# Get the latest abbreviated commit hash of the working branch
-execute_process(
-  COMMAND git log -1 --format=%h
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE ${PROJECT_NAME}_GIT_COMMIT_HASH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if( NOT CMDLINEDEFS MATCHES "${PROJECT_NAME}_GIT_BRANCH" )
+	# Get the current working branch
+	execute_process(
+	  COMMAND git rev-parse --abbrev-ref HEAD
+	  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	  OUTPUT_VARIABLE ${PROJECT_NAME}_GIT_BRANCH
+	  OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	# make the definition visible to the preprocessor
+	add_compile_definitions("${PROJECT_NAME}_GIT_BRANCH=${${PROJECT_NAME}_GIT_BRANCH}")
+endif()
 
-add_definitions("-D${PROJECT_NAME}_GIT_COMMIT_HASH=${${PROJECT_NAME}_GIT_COMMIT_HASH}")
-add_definitions("-D${PROJECT_NAME}_GIT_BRANCH=${${PROJECT_NAME}_GIT_BRANCH}")
+if( NOT CMDLINEDEFS MATCHES "${PROJECT_NAME}_GIT_COMMIT_HASH" )
+	# Get the latest abbreviated commit hash of the working branch
+	execute_process(
+	  COMMAND git log -1 --format=%h
+	  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	  OUTPUT_VARIABLE ${PROJECT_NAME}_GIT_COMMIT_HASH
+	  OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	# make the definition visible to the preprocessor
+	add_compile_definitions("${PROJECT_NAME}_GIT_COMMIT_HASH=${${PROJECT_NAME}_GIT_COMMIT_HASH}")
+endif()
